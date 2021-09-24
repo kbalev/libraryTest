00831e17209288705c61dae0b5e78bb06145265c
kbalev <kamen_balev@yahoo.com>
2021-09-24 12:29:59 +0100

    digital now has some extra code

--- src/main/java/Alexandria/Digital.java
+++ src/main/java/Alexandria/Digital.java
@@ -2,6 +2,14 @@ package Alexandria;
 
 public abstract class Digital extends Inventory {
 
+
+    public Digital(){
+        super(false);
+    }
+    public Digital(boolean borrowed){
+        super(borrowed);
+    }
+
     public String requiredDevice(){
         return "I require a computer to be viewed";
     };
